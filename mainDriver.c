907642f2abd4a02efbbd73b21f4102c0a077e29b
Gustavo Barreto <goostavobg@gmail.com>
2011-12-03 22:40:58 -0200

    Corrigido pequeno erro.

--- mainDriver.c
+++ mainDriver.c
@@ -3,16 +3,41 @@
 //Main para testes do driver
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "driver.h"
 
 int main(void)
 {
-    int temp;
+    int temp, temp2;
+    char* buffer;
+    buffer = (char*)malloc(4096);
     temp =driver_init("Teste_log");
     printf("\n\nInicializado com status:%d\n",temp);
     temp=driver_blcksize();
     printf("Block size:%d\n",temp);
     temp=driver_blckcount();
     printf("Block count:%d\n",temp);
+
+    for (temp=0;temp<511;temp++)
+    {
+        buffer[temp] = (char)temp;
+    }
+    buffer[511] = '\0';
+
+    for(temp=1;temp<10;temp++)
+    {
+        driver_write(temp,buffer);
+    }
+
+    for(temp=1;temp<10;temp++)
+    {
+        for (temp2=0;temp2<512;temp2++)
+        {
+            buffer[temp] = 0;
+        }
+        driver_read(temp,buffer);
+        printf("%s\n",buffer);
+    }
+
     return 0;
 }
